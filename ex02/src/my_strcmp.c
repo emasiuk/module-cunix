049d39fa232b60b257f065f9ec8ef6991f61c25b
Radwimp <Geass23621@gmail.com>
2018-06-21 11:25:44 +0300

    Add ex02

---
+++ ex02/src/my_strcmp.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+int my_strcmp(char *str1, char *str2)
+{
+  int i;
+
+  i = 0;
+  while (str1[i] != '\0')
+  {
+    if (str1[i] < str2[i])
+      return -1;
+    else if (str1[i] > str2[i])
+      return 1;
+    i = i + 1;
+  }
+  if (str2[i] != '/0')
+    return 1;
+  else
+    return 0;
+}
