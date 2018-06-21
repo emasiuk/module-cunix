4fc5a9c773e1b53d55e8b13486adbea24e6fa591
Radwimp <Geass23621@gmail.com>
2018-06-21 15:40:52 +0300

    Add ex05

---
+++ ex05/src/my_puts.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+
+int my_puts(const char *s)
+{
+  int i;
+  char c;
+
+  i = -1;
+
+
+  while (++i < strlen(s))
+  {
+    c = s[i];
+    write(1, &c, 1);
+  }
+  c = '\n';
+  write (1, &c, 1);
+  return 0;
+}
